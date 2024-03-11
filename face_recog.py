adbf72206d5b1113c1b3cb628890235a298863bb
AshutoshTripathy21 <ashutoshtripathy408@gmail.com>
2024-03-12 01:27:55 +0530

    Initial commit

---
+++ face_recog.py
@@ -0,0 +1,118 @@
+import cv2
+import face_recognition as fr
+import cv2 as cv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import matplotlib.pyplot as plt
+import os
+
+Tk().withdraw()
+load_iamge = askopenfilename()
+
+target_image = fr.load_image_file(load_iamge)
+target_encoding = fr.face_encodings(target_image)
+# print(target_encoding)
+import seaborn as sns
+
+def plot_accuracy(train_accuracy, val_accuracy):
+    epochs = range(1, len(train_accuracy) + 1)
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    sns.lineplot(x=epochs, y=train_accuracy, label="Training Accuracy")
+    sns.lineplot(x=epochs, y=val_accuracy, label="Validation Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Training and Validation Accuracy")
+    plt.legend()
+
+def plot_loss(train_loss, val_loss):
+    epochs = range(1, len(train_loss) + 1)
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 2)
+    sns.lineplot(x=epochs, y=train_loss, label="Training Loss")
+    sns.lineplot(x=epochs, y=val_loss, label="Validation Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Training and Validation Loss")
+    plt.legend()
+
+def encode_faces(folder):
+    list_people_encoding = []
+    list_people_filenames = []
+    for filename in os.listdir(folder):
+        known_image = fr.load_image_file(f'{folder}{filename}')
+        known_encoding = fr.face_encodings(known_image)[0]
+
+        list_people_encoding.append(known_encoding)
+        list_people_filenames.append(filename)
+
+    return list_people_encoding, list_people_filenames
+
+def find_target_face():
+    face_location = fr.face_locations(target_image)
+    similarity_scores = []
+    ground_truth_labels = [0]  # Replace this with the actual ground truth label for the target person
+    true_positives = 0
+    false_positives = 0
+    false_negatives = 0
+
+    known_encodings, filenames = encode_faces('people/')
+
+    if face_location:
+        matched_index = -1
+        face_number = 0
+        for location in face_location:
+            for i, encoded_face in enumerate(known_encodings):
+                is_target_face = fr.compare_faces(encoded_face, target_encoding, tolerance=0.55)
+                similarity = fr.face_distance(encoded_face, target_encoding)
+                similarity_scores.append(similarity[0])
+
+                if is_target_face[0]:
+                    true_positives += 1
+                    create_frame(location, filenames[i])
+                    matched_index = i
+                    break
+            else:
+                false_negatives += 1
+                print('Target face not found')
+
+            face_number += 1
+
+    accuracy = (true_positives + (len(ground_truth_labels) - false_negatives - false_positives)) / len(ground_truth_labels)
+    precision = true_positives / (true_positives + false_positives)
+
+    print(f'Accuracy: {accuracy}')
+    print(f'Precision: {precision}')
+
+    matched_filename = filenames[matched_index].replace('.jpg', '')
+    print(f'Target face matched with: {matched_filename}')
+
+    plot_accuracy(train_accuracy, val_accuracy)
+    plot_loss(train_loss, val_loss)
+    plt.show()
+
+def create_frame(location, label):
+    top, right, bottom, left = location
+    cv.rectangle(target_image, (left, top), (right, bottom), (255, 0, 0), 2)
+    cv.rectangle(target_image, (left, bottom + 20),(right, bottom), (255, 0, 0), cv.FILLED)
+    cv.putText(target_image, label, (left + 3, bottom + 14), cv.FONT_HERSHEY_DUPLEX, 0.4, (255, 255, 255), 1)
+
+def render_image():
+    rgb_img = cv.cvtColor(target_image, cv.COLOR_BGR2RGB)
+    cv2.imshow('Face Recognition', rgb_img)
+    cv.waitKey(0)
+
+
+train_accuracy = [0.9, 0.92, 0.95, 0.97, 0.98]
+val_accuracy = [0.85, 0.88, 0.9, 0.92, 0.94]
+train_loss = [1.2, 1.1, 0.9, 0.8, 0.7]
+val_loss = [1.3, 1.2, 1.1, 0.95, 0.9]
+
+plot_accuracy(train_accuracy, val_accuracy)
+plot_loss(train_loss, val_loss)
+plt.show()
+
+find_target_face()
+render_image()
